68b2546292753513e2fbec7ceb605e547ca02ba1
john.gunter <john.gunter@kogerconsulting.com>
2021-10-07 19:23:02 +0100

    add unit test folder

---
+++ BPCalculator/BPUnitTestProject/BPUnitTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BPCalculator;
+
+namespace bpUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+     
+            Assert.AreEqual(2, 1+1 );
+        }
+
+     
+    }
+}
