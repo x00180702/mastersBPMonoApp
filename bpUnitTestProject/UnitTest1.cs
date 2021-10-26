b1abf35aef3979c5c5caed385be64650604232cd
john.gunter <john.gunter@kogerconsulting.com>
2021-10-26 18:12:09 +0100

    add unit tests

--- bpUnitTestProject/UnitTest1.cs
+++ bpUnitTestProject/UnitTest1.cs
@@ -1,27 +1,33 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using BPCalculator;
-using System;
+﻿using BPCalculator;
 using Xunit;
 
 namespace bpUnitTestProject
 {
 
-    [TestClass]
-
     public class UnitTest1
     {
         public BloodPressure BP;
-
-        [TestMethod]
         [Theory]
         [InlineData(70,40)] //low range
         [InlineData(82,48)] //mid range
         [InlineData(89,59)] //high range
-        public void TestMethod1(int s, int d)
+        public void TestMethodLowVariables(int s, int d)
         {
       
             BP = new BloodPressure() { Systolic = s, Diastolic = d };
-            Assert.AreEqual(BPCategory.Low, BP.Category);
+            Assert.Equal(BPCategory.Low, BP.Category);
+
+        }
+
+        [Theory]
+        [InlineData(90, 60)] //low range
+        [InlineData(105, 62)] //mid range
+        [InlineData(119, 79)] //high range
+        public void TestMethodIdealVariables(int s, int d)
+        {
+
+            BP = new BloodPressure() { Systolic = s, Diastolic = d };
+            Assert.Equal(BPCategory.Ideal, BP.Category);
 
         }
     }
