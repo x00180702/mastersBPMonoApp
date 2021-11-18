79a671a3b205beb5cb288fdd08b71ce32fb57fe5
john.gunter <john.gunter@kogerconsulting.com>
2021-11-18 19:16:50 +0000

    add selenium

---
+++ BPCalculatorAcceptanceTests/Drivers/browserDrivers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BPCalculatorAcceptanceTests.Drivers
+{
+    public class browserDrivers
+   : IDisposable
+    {
+        private readonly Lazy<IWebDriver> _currentWebDriverLazy;
+        private bool _isDisposed;
+
+        public browserDrivers()
+        {
+            _currentWebDriverLazy = new Lazy<IWebDriver>(CreateWebDriver);
+        }
+
+        /// <summary>
+        /// The Selenium IWebDriver instance
+        /// </summary>
+        public IWebDriver Current => _currentWebDriverLazy.Value;
+
+        /// <summary>
+        /// Creates the Selenium web driver (opens a browser)
+        /// </summary>
+        /// <returns></returns>
+        private IWebDriver CreateWebDriver()
+        {
+            //We use the Chrome browser
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+
+            var chromeOptions = new ChromeOptions();
+
+            var chromeDriver = new ChromeDriver(chromeDriverService, chromeOptions);
+
+            return chromeDriver;
+        }
+
+        /// <summary>
+        /// Disposes the Selenium web driver (closing the browser)
+        /// </summary>
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (_currentWebDriverLazy.IsValueCreated)
+            {
+                Current.Quit();
+            }
+
+            _isDisposed = true;
+        }
+    }
+}
---
+++ BPCalculatorAcceptanceTests/Steps/BloodPressureCalcultorTestsSteps.cs
@@ -0,0 +1,50 @@
+﻿using BPCalculatorAcceptanceTests.PageObjects;
+using BPCalculatorAcceptanceTests.Drivers;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace BPCalculatorAcceptanceTests.Steps
+{
+    [Binding]
+    public class BloodPressureCalcultorTestsSteps
+    {
+
+        private readonly BPCalculatorObjects _calculatorPageObject;
+
+        public BloodPressureCalcultorTestsSteps(browserDrivers browserDriver)
+        {
+            _calculatorPageObject = new BPCalculatorObjects(browserDriver.Current);
+        }
+
+        [Given(@"is on the Blood Pressure Calcultor Page")]
+        public void GivenIsOnTheBloodPressureCalcultorPage()
+        {
+            _calculatorPageObject.goToWebsite();
+        }
+
+        [Given(@"the user enters (.*) in the Systolic field on the Blood Pressure Calcultor Page")]
+        public void GivenTheUserEntersInTheSystolicFieldOnTheBloodPressureCalcultorPage(string systolic)
+        {
+            _calculatorPageObject.enterSystolicNumber(systolic);
+        }
+
+        [Given(@"the user enters (.*) in the Diastolic field on the Blood Pressure Calcultor Page")]
+        public void GivenTheUserEntersInTheDiastolicFieldOnTheBloodPressureCalcultorPage(string diastolic)
+        {
+            _calculatorPageObject.enterDiastolicNumber(diastolic);
+        }
+
+        [When(@"the clicks on the submit button on the Blood Pressure Calcultor Page")]
+        public void WhenTheClicksOnTheSubmitButtonOnTheBloodPressureCalcultorPage()
+        {
+            _calculatorPageObject.clickSubmitButton();
+        }
+
+        [Then(@"the result (.*) will be diplayed on the Blood Pressure Calcultor Page")]
+        public void ThenTheResultLowBloodPressureWillBeDiplayedOnTheBloodPressureCalcultorPage(string expectedValue)
+        {
+            string actualValue = _calculatorPageObject.getResults();
+            Assert.AreEqual(actualValue, expectedValue);
+        }
+    }
+}
