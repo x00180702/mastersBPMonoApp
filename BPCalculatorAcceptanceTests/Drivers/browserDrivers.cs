2a58490de67324a32a92c90b58156641e7ad2c2c
john.gunter <john.gunter@kogerconsulting.com>
2021-11-23 07:55:29 +0000

    add settings

--- BPCalculatorAcceptanceTests/Drivers/browserDrivers.cs
+++ BPCalculatorAcceptanceTests/Drivers/browserDrivers.cs
@@ -34,7 +34,13 @@ namespace BPCalculatorAcceptanceTests.Drivers
             chromeOptions.AddUserProfilePreference("disable-popup-blocking", "true");
             chromeOptions.AddUserProfilePreference("download.prompt_for_download", false);
             chromeOptions.AddArguments("disable-infobars");
-   
+            chromeOptions.AddArguments("start-maximized"); // open Browser in maximized mode
+            chromeOptions.AddArguments("disable-infobars"); // disabling infobars
+            chromeOptions.AddArguments("--disable-extensions"); // disabling extensions
+            chromeOptions.AddArguments("--disable-gpu"); // applicable to windows os only
+            chromeOptions.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            chromeOptions.AddArguments("--no-sandbox"); // Bypass OS security model
+
 
             var chromeDriver = new ChromeDriver(chromeDriverService, chromeOptions);
 
