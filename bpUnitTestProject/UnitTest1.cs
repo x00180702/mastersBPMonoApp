3f6dc41f2a11f2c3119583cb3a09816baf0a9c35
john.gunter <john.gunter@kogerconsulting.com>
2021-12-01 08:53:05 +0000

    add unit test

--- bpUnitTestProject/UnitTest1.cs
+++ bpUnitTestProject/UnitTest1.cs
@@ -65,6 +65,7 @@ namespace bpUnitTestProject
         [InlineData(195, 39)] //low range
         [InlineData(200, 23)] //mid range
         [InlineData(191, 101)] //high range
+        [InlineData(40, 60)] //Systolic lower then Diastolic
         public void TestMethodInvalidVariables(int s, int d)
         {
 
