ebdf5a58f46fb5443748e1298df25f33adf860a1
john.gunter <john.gunter@kogerconsulting.com>
2021-12-03 20:56:18 +0000

    add

--- BPCalculator/Startup.cs
+++ BPCalculator/Startup.cs
@@ -19,8 +19,6 @@ namespace BPCalculator
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-          
-            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
