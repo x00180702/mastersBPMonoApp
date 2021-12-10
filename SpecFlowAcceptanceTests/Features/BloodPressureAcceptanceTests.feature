79b6ebe6b64d4b882134b3eda090fde3973fd449
john.gunter <john.gunter@kogerconsulting.com>
2021-12-10 18:43:17 +0000

    add

--- SpecFlowAcceptanceTests/Features/BloodPressureAcceptanceTests.feature
+++ SpecFlowAcceptanceTests/Features/BloodPressureAcceptanceTests.feature
@@ -6,11 +6,8 @@ Scenario Outline: Check value for calculations
 	And user enters <Diastolic> in Diastolic
 	Then the result should be <Result>
 
-
 	Examples: 
     | Systolic	| Diastolic |	Result		|
     |    70		|   59		|   Low 		|
-    |    119	|   60		|   Ideal 		|
-	|    121	|   89		|   PreHigh		|
 	|    190	|   90		|   High 		|
 	|    195	|   101		|   NotValid 	|
--- SpecFlowAcceptanceTests/Features/BloodPressureAcceptanceTests.feature.cs
+++ SpecFlowAcceptanceTests/Features/BloodPressureAcceptanceTests.feature.cs
@@ -127,6 +127,46 @@ this.ScenarioInitialize(scenarioInfo);
             }
             this.ScenarioCleanup();
         }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Check value for calculations - Ideal")]
+        public virtual void CheckValueForCalculations_Ideal()
+        {
+            string[] tagsOfScenario = ((string[])(null));
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Check value for calculations - Ideal", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 17
+ this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 18
+ testRunner.Given("user enters 119 in Systolic", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 19
+ testRunner.And("user enters 60 in Diastolic", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 20
+ testRunner.Then("the result should be Ideal", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
     }
 }
 #pragma warning restore
