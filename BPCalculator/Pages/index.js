883be07e1d2afbe8cc44aa599d9349f241c42666
x00180702 <90932427+x00180702@users.noreply.github.com>
2022-10-13 15:39:43 +0100

    Update index.js

--- BPCalculator/Pages/index.js
+++ BPCalculator/Pages/index.js
@@ -1,3 +1,10 @@
-setTimeout(() => {
-  document.location.reload();
-}, 3000);
+$(document).ready(function() {
+  setInterval(function() {
+
+  }, 3000);
+});
+
+function cache_clear() {
+  //window.location.reload(true);
+  window.location.reload(); use this if you do not remove cache
+}
